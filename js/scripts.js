37177f9cfb5efcac91a8c2e44d21042af61b9f1a
Artur Weinstein <artur.weinstein@gmail.com>
2021-12-10 14:31:21 -0800

    create number generator that generates planet earth mass

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 var planetTraits = ["Rocky", "Icy", "Gas Giant", "Earth-Like"];
-
+var planetAtmosphere = ["Nitrogen Rich", "Oxygen Rich", "Amonia", "Helium", "Carbon Dioxide"];
+//planet temp?
 function planetType(){
   let planet = planetTraits[Math.floor(Math.random() * planetTraits.length)];
   return planet;
 }
 
+function earthMasses(){
+  var num = 100;
+  var mass = Math.floor(Math.random() * (10 * num - 1 * num) + 1 * num) / (1*num);
+  return mass;
+}
 
 function systemName() {
   var text = "";
@@ -16,11 +22,17 @@ function systemName() {
   return text;
 }
 
-
 $(document).ready(function() {
   $('.submit').click(function(event) {
     event.preventDefault();
       $(this).html(systemName());
       $('.planet').text(planetType());
+  })
+});
+
+$(document).ready(function(){
+  $('.planet').click(function(event){
+    event.preventDefault();
+      $('#masses').text(earthMasses());
   })
 });
