1431ba949727982b076850e1fe8901074cde5007
Art Weinstein <artur.weinstein@gmail.com>
2021-12-10 08:43:11 -0800

    delete react files, start project fresh with javascript and find blender resource.

--- spacecap/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-export default function App() {
-  return <Canvas></Canvas>;
-}
--- spacecap/src/planetData.js
+++
@@ -1,4 +0,0 @@
-const random = (a, b) => a + Math.random()*b;
-const randomInt = (a, b) => Math.floor(random(a, b));
-const randomColor = () => 
-  `rgb(${randomInt(80, 50)}, ${randomInt(80, 50)}, ${randomInt(80, 50)})`;
