16561f97f1a3e224524620b46699979cc7ac82a6
Artur Weinstein <artur.weinstein@gmail.com>
2021-12-10 15:48:55 -0800

    WIP store random variables for each planet

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 var planetTraits = ["Rocky", "Icy", "Gas Giant", "Earth-Like"];
 var planetAtmosphere = ["Nitrogen Rich", "Oxygen Rich", "Amonia", "Helium", "Carbon Dioxide"];
 //planet temp?
-var finalPlanet = [];
 function planetType(){
   let planet = planetTraits[Math.floor(Math.random() * planetTraits.length)];
   return planet;
 }
 
 function earthMasses(){
+  var finalMass = [];
   var num = 100;
-  var mass = Math.floor(Math.random() * (10 * num - 1 * num) + 1 * num) / (1*num);
-  return mass;
+  finalMass.push(Math.floor(Math.random() * (10 * num - 1 * num) + 1 * num) / (1*num));
+  return finalMass;
 }
 
 function systemName() {
@@ -23,15 +23,20 @@ function systemName() {
   return text;
 }
 
+function finalPlanet(){
+  return finalMass;
+}
 
 $(document).ready(function() {
   $('.submit').click(function(event) {
     event.preventDefault();
       $(this).html(systemName());
       $('.planet0').text(planetType());
+      $('.masses').text(earthMasses());
       $('.planet1').text(planetType());
+      $('.masses').text(earthMasses());
       $('.planet2').text(planetType());
-      $('#masses').text(earthMasses());
+      $('.masses').text(earthMasses());
 
   })
 });
