59b1bf4e27e07c6c9d036d932ec61f4fc107838e
Artur Weinstein <artur.weinstein@gmail.com>
2021-12-20 18:54:50 -0800

    add icy planet model to show when toggling icy planet button

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var play = document.getElementById('play');
 var count = 0;
 
 
-function playPause(){
-  if(count==0){
+function playPause() {
+  if (count == 0) {
     count = 1
     audio.play();
-  } else{
+  } else {
     count = 0;
     audio.pause();
   }
@@ -22,7 +22,7 @@ function planetType() {
   return planet;
 }
 
-function planetAtm(){
+function planetAtm() {
   let planetAt = planetAtmosphere[Math.floor(Math.random() * planetAtmosphere.length)];
   return "Primary atmosphere composition: " + planetAt;
 }
@@ -47,9 +47,9 @@ function finalPlanet() {
   return finalMass;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   var btn = $(".play");
-  btn.click(function() {
+  btn.click(function () {
     btn.toggleClass("paused");
     return false;
   });
@@ -57,14 +57,14 @@ $(document).ready(function() {
 
 
 $(document).ready(function () {
-  $('.continue').click(function (event){
+  $('.continue').click(function (event) {
     event.preventDefault();
     $('.initial').hide();
     $('.next').fadeIn();
     $('.begin').fadeIn();
 
   })
-  $('.begin').click(function (event){
+  $('.begin').click(function (event) {
     event.preventDefault();
     $('.next').hide();
     $('.submit0').html(systemName());
@@ -76,7 +76,7 @@ $(document).ready(function () {
   $('.submit0, .submit1, .submit2').click(function (event) {
     event.preventDefault();
     var num = Math.random();
-    if (num <0.1){
+    if (num < 0.1) {
       $('.displayRogue').show();
       $('.planet0').hide()
       $('.planet1').hide()
@@ -99,26 +99,57 @@ $(document).ready(function () {
   })
   $('.planet0').click(function (event) {
     event.preventDefault();
-    if($(this).text()=='Gas Giant') {
-      $('.testPlanet').fadeToggle();
-     } else {
-       $('.testPlanet').hide();
-     }
-    // $('.testPlanet').toggle();
+    if ($(this).text() == 'Gas Giant') {
+      $('#gasPlanet0').fadeToggle();
+    } else {
+      $('#gasPlanet0').hide();
+    }
+    if ($(this).text() == 'Icy') {
+      $('#icePlanet0').fadeToggle();
+    } else {
+      $('#icePlanet0').hide();
+    }
+    // $('.gasPlanet').toggle();
     $('.masses0').fadeToggle();
     $('.atmosphere0').fadeToggle();
   })
   $('.submit0, .submit1, .submit2').click(function (event) {
     event.preventDefault()
-    $('.testPlanet').hide();
+    $('#gasPlanet0').hide();
+    $('#gasPlanet1').hide();
+    $('#gasPlanet2').hide();
+    $('#icePlanet0').hide();
+    $('#icePlanet1').hide();
+    $('#icePlanet2').hide();
   })
   $('.planet1').click(function (event) {
     event.preventDefault();
+    if ($(this).text() == 'Gas Giant') {
+      $('#gasPlanet1').fadeToggle();
+    } else {
+      $('#gasPlanet1').hide();
+    }
+    if ($(this).text() == 'Icy') {
+      $('#icePlanet1').fadeToggle();
+    } else {
+      $('#icePlanet1').hide();
+    }
+
     $('.masses1').toggle();
     $('.atmosphere1').toggle();
   })
   $('.planet2').click(function (event) {
     event.preventDefault();
+    if ($(this).text() == 'Gas Giant') {
+      $('#gasPlanet2').fadeToggle();
+    } else {
+      $('#gasPlanet2').hide();
+    }
+    if ($(this).text() == 'Icy') {
+      $('#icePlanet2').fadeToggle();
+    } else {
+      $('#icePlanet2').hide();
+    }
     $('.masses2').toggle();
     $('.atmosphere2').toggle();
   })
